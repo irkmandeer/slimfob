bf40b0fc64f7f3d15c770ecc838002556bb4716d
Irkmandeer <37556625+irkmandeer@users.noreply.github.com>
2023-12-15 19:27:55 +0200

    Ensure plugins are loaded after registry.

--- bot.js
+++ bot.js
@@ -1,19 +1,11 @@
 // process.env.DEBUG = 'minecraft-protocol'
 require('util').inspect.defaultOptions.depth = 10
 
-
 const { pathfinder } = require('mineflayer-pathfinder')
-
-
-
 const { createBot } = require('./lib/loader')
 
 const auth = require('./credentials')
 const options = {
-
-    // version: '1.20.0',
-    version: '1.20.50',
-
 }
 const bot = createBot({ ...auth, ...options })
 
@@ -28,6 +20,11 @@ process.on('SIGINT', (signal) => {
 
     console.log('(SIGINT)')
     bot.quit('Quit')
+})
+
+bot.on('blockUpdate', (oldBlock, newBlock) => {
+
+    console.log('Block Updated:', { block: newBlock?.name, position: newBlock.position })
 })
 
 
