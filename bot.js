e41cb48d4439296c4805d1580765a0dab99f6ae8
Irkmandeer <37556625+irkmandeer@users.noreply.github.com>
2023-12-16 18:14:31 +0200

    Added and removed debug logging

--- bot.js
+++ bot.js
@@ -27,5 +27,39 @@ bot.on('blockUpdate', (oldBlock, newBlock) => {
     console.log('Block Updated:', { block: newBlock?.name, position: newBlock.position })
 })
 
+bot._client.on('spawn', (packet) => {
+
+    // console.log('spawn', bot.entity, bot.username)
+    console.log('Spawn', bot.entity.position)
+
+})
+
+bot.on('playerJoined', (player) => {
+
+    console.log(player.username, 'joined the game.', player.entity?.position)
+})
 
 
+bot.on('playerLeft', (player) => {
+
+    console.log(player.username, 'left the game.', player.entity?.position)
+})
+
+bot.on('entitySpawn', (entity) => {
+
+    console.log('entitySpawn', entity.type, entity.username ?? entity.name)
+})
+
+
+bot.on('entityGone', (entity) => {
+
+    console.log('entityGone', entity.type, entity.username ?? entity.name)
+})
+
+bot._client.on('move_player', (packet) => {
+
+    if (packet.runtime_id === bot._client.entityId) {
+
+        console.log('move_player', packet, bot._client.entityId)
+    }
+})
